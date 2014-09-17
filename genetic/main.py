a4b49e68f8b27cf697e61f3c29e220a1ffcbb50b
unknown <gd74@CHEK-0213-09.SGF.EDUBEAR.NET>
2014-09-17 12:52:52 -0500

    Added logging and scheduler class

--- genetic/main.py
+++ genetic/main.py
@@ -1,8 +1,18 @@
 from __future__ import print_function
+from scheduler import Scheduler
+
 import unittest
+import logging
+
+
+def init_logging():
+    logging.basicConfig(filename='genetic.log', level=logging.DEBUG)
+    logging.debug("Logging is initialized")
+
 
 def main():
-    print("Hello world!")
+    init_logging()
+    s = Scheduler()
 
 
 if __name__ == "__main__":
---
+++ genetic/scheduler.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import logging
+
+class Scheduler:
+    def __init__(self):
+        logging.debug("Scheduler init")
+
+
+    def calc_fitness(self):
+        pass
+
+
+    def mutate(self):
+        pass
+
+
+    def breed(self):
+        pass
+
+
+    def evolution_loop(self):
+        pass
