88dedc8c869a20ef190635bf136555b88baeb488
Daniel Hebrink <dh1991@live.missouristate.edu>
2014-10-01 11:54:52 -0500

    Change to underscore naming convention

--- mainPage.py
+++ mainPage.py
@@ -4,64 +4,64 @@ from guiClasses import *
 
 
 # Define click functions
-def clickHome():
-    topLabelText.set("Home Screen")
+def click_home():
+    top_label_text.set("Home Screen")
     return
 
-def clickConstraint():
-    topLabelText.set("Constraint Screen")
+def click_constraint():
+    top_label_text.set("Constraint Screen")
     return
 
-def clickView():
-    topLabelText.set("View Screen")
+def click_view():
+    top_label_text.set("View Screen")
     return
 
-def clickMisc():
-    topLabelText.set("Misc Screen")
+def click_misc():
+    top_label_text.set("Misc Screen")
     return
 
-def clickRun():
+def click_run():
     # run the scheduler
-    topLabelText.set("Scheduler should be running...")
+    top_label_text.set("Scheduler should be running...")
     return
 
 
-mainWindow = Tk()
+main_window = Tk()
 
-windowWidth = 850
-windowHeight = 600
-screenXpos = (mainWindow.winfo_screenwidth() / 2) - (windowWidth / 2)
-screenYpos = (mainWindow.winfo_screenheight() / 2) - (windowHeight / 2)
+window_width = 850
+window_height = 600
+screen_x_pos = (main_window.winfo_screenwidth() / 2) - (window_width / 2)
+screen_y_pos = (main_window.winfo_screenheight() / 2) - (window_height / 2)
 
-mainWindow.geometry(str(windowWidth) + 'x' + str(windowHeight) +\
-                    '+' + str(screenXpos) + '+' + str(screenYpos))
+main_window.geometry(str(window_width) + 'x' + str(window_height) +\
+                    '+' + str(screen_x_pos) + '+' + str(screen_y_pos))
 
 # Next two lines are using guiClasses.py and TopLevel for sub-windows
-main = MainWindow(mainWindow)
-main.pack()
+#main = MainWindow(main_window)
+#main.pack()
 
 # Following section places everything on one window
-'''
-mainWindow.title('CSC Scheduler')
 
-topLabelText = StringVar()
-topLabelText.set("You have just begun!")
-topLabel = Label(mainWindow, textvariable = topLabelText)
-topLabel.grid(row = 0, column = 2)
+main_window.title('CSC Scheduler')
 
-homeButton = Button(mainWindow, text = "Home", width = 15, height = 5, command = clickHome)
-homeButton.grid(row = 0, column = 0)
+top_label_text = StringVar()
+top_label_text.set("You have just begun!")
+top_label = Label(main_window, textvariable = top_label_text)
+top_label.grid(row = 0, column = 2)
 
-constraintButton = Button(mainWindow, text = "Constraint", width = 15, height = 5, command = clickConstraint)
-constraintButton.grid(row = 1, column = 0)
+home_button = Button(main_window, text = "Home", width = 15, height = 5, command = click_home)
+home_button.grid(row = 0, column = 0)
 
-viewButton = Button(mainWindow, text = "View", width = 15, height = 5, command = clickView)
-viewButton.grid(row = 2, column = 0)
+constraint_button = Button(main_window, text = "Constraint", width = 15, height = 5, command = click_constraint)
+constraint_button.grid(row = 1, column = 0)
 
-miscButton = Button(mainWindow, text = "...", width = 15, height = 5, command = clickMisc)
-miscButton.grid(row = 3, column = 0)
+view_button = Button(main_window, text = "View", width = 15, height = 5, command = click_view)
+view_button.grid(row = 2, column = 0)
 
-runButton = Button(mainWindow, text = "RUN", width = 15, height = 10, bg = "green", command = clickRun)
-runButton.grid(row = 4, column = 0)
-'''
-mainWindow.mainloop()   # NEED FOR MAC OSX AND WINDOWS
+misc_button = Button(main_window, text = "...", width = 15, height = 5, command = click_misc)
+misc_button.grid(row = 3, column = 0)
+
+run_button = Button(main_window, text = "RUN", width = 15, height = 10, bg = "green", command = click_run)
+run_button.grid(row = 4, column = 0)
+
+main_window.mainloop()   # NEED FOR MAC OSX AND WINDOWS
