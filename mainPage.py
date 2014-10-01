4627c68701f1651e1f48e1de825ad6086c012583
Daniel Hebrink <dh1991@live.missouristate.edu>
2014-10-01 17:18:49 -0500

    Remove mainPagenew

--- mainPage.py
+++ mainPage.py
@@ -6,6 +6,7 @@ from guiClasses import *
 # Define click functions
 def click_home():
     top_label_text.set("Home Screen")
+    content_frame.delete(ALL)
     return
 
 def click_constraint():
@@ -25,6 +26,15 @@ def click_run():
     top_label_text.set("Scheduler should be running...")
     return
 
+def add_course_con():
+    top_label_text.set("course constraint")
+    return
+
+def add_instructor_con():
+    top_label_text.set("Instructor constraint")
+    return
+
+
 
 main_window = Tk()
 
@@ -40,28 +50,48 @@ main_window.geometry(str(window_width) + 'x' + str(window_height) +\
 ##main = MainWindow(main_window)
 ##main.pack()
 
-# Following section places everything on one window
+# Following section places everything on one windowFO301028
 
 main_window.title('CSC Scheduler')
 
 top_label_text = StringVar()
 top_label_text.set("You have just begun!")
-top_label = Label(main_window, textvariable = top_label_text)
-top_label.grid(row = 0, column = 2)
 
-home_button = Button(main_window, text = "Home", width = 15, height = 5, command = click_home)
-home_button.grid(row = 0, column = 0)
+# MENU SECTION
+menu_frame = Frame(main_window, bg = 'red')
+menu_frame.grid(row = 0, column = 0, rowspan = 6, columnspan = 2)
 
-constraint_button = Button(main_window, text = "Constraint", width = 15, height = 5, command = click_constraint)
-constraint_button.grid(row = 1, column = 0)
+# MENU BUTTONS
+home_button = Button(menu_frame, text = "Home", width = 15, height = 5, command = click_home)
+home_button.pack()
 
-view_button = Button(main_window, text = "View", width = 15, height = 5, command = click_view)
-view_button.grid(row = 2, column = 0)
+constraint_button = Button(menu_frame, text = "Constraint", width = 15, height = 5, command = click_constraint)
+constraint_button.pack()
 
-misc_button = Button(main_window, text = "...", width = 15, height = 5, command = click_misc)
-misc_button.grid(row = 3, column = 0)
+view_button = Button(menu_frame, text = "View", width = 15, height = 5, command = click_view)
+view_button.pack()
 
-run_button = Button(main_window, text = "RUN", width = 15, height = 10, bg = "green", command = click_run)
-run_button.grid(row = 4, column = 0)
+misc_button = Button(menu_frame, text = "...", width = 15, height = 5, command = click_misc)
+misc_button.pack()
+
+run_button = Button(menu_frame, text = "RUN", width = 15, height = 10, bg = "green", command = click_run)
+run_button.pack()
+
+
+# CONTENT SECTION
+content_frame = Canvas(main_window)
+content_frame.grid(row = 0, column = 2, rowspan = 6, columnspan = 5)
+
+top_label = Label(content_frame, textvariable = top_label_text)
+top_label.pack(side = TOP)
+
+add_course_con = Button(content_frame, text = "add course constraint", width =15, height =5, command = add_course_con)
+add_course_con.pack(side = LEFT, padx = 2, pady = 2)
+
+add_instructor_con = Button(content_frame, text = "add instructor constraint", width =15, height =5, command = add_instructor_con )
+add_instructor_con.pack(side = LEFT, padx = 5, pady = 2)
+
+
 
 main_window.mainloop()   # NEED FOR MAC OSX AND WINDOWS
+
