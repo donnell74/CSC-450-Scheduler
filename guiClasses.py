f8a58c42fc173eea8b03903ea9853894ca8e87d3
fquashie <fquashie@gmail.com>
2014-10-06 12:06:10 -0500

    constraint page

--- guiClasses.py
+++ guiClasses.py
@@ -39,6 +39,44 @@ class ConstraintPage(Page):
         self.head_label = Label(self, text="Constraint Page")
         self.head_label.pack()
 
+        paragraph_text = "This is constraint"
+        self.description_label = Label(self, text=paragraph_text, font=(font_style, size_p))
+        self.description_label.pack()
+
+        
+
+        self.menu = Frame(self, background ="bisque", width="500", height="600")
+        self.menu.pack(side="top", fill="both", expand=True)
+
+        self.top_frame= Frame(self.menu, background="green")
+        self.bottom_frame =Frame(self.menu, background="yellow")
+        self.top_frame.pack(side="top", fill="x", expand=False)
+        self.bottom_frame.pack(side="bottom", fill="both", expand=True)
+
+##        self.top_left =Frame(self.menu, text="Top Left")
+##        self.top_right =Frame(self.menu, text="Top Right")
+        self.top_left =Frame(self.menu, background ="pink")
+        self.top_right =Frame(self.menu, background="blue")
+        self.top_left.pack(side="left", fill="x", expand =True)
+        self.top_right.pack(side="right", fill="x", expand=True)
+
+        self.top_left_label = Label(self.top_left, text="Top Left")
+        self.top_right_label = Label(self.top_right, text="Top Right")
+        self.top_left_label.pack(side="left")
+        self.top_right_label.pack(side = "right")
+
+        self.text_box =Text(self.bottom_frame, height=10, width=50, background="grey")
+        self.text_box.pack(side="top", fill="both", expand=True)
+        
+
+        self.add_con_btn = Button(self.menu, text="add course constraint", width="20", height="5")
+        self.add_con_btn.pack(fill =X, side="left", pady=50, padx=50)
+
+        self.add_instructor_btn = Button(self.menu, text="add instructor constraint", width="20", height="5")
+        self.add_instructor_btn.pack(fill =X, side="right", pady=50, padx=50)
+
+        
+
 class ViewPage(Page):
 
     def __init__(self, root):
@@ -119,3 +157,4 @@ class MainWindow(Frame):
 
     def run_scheduler(self):
         return
+
