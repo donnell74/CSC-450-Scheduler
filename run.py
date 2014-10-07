95d315399535043f04a8243650b188754afd4840
Gregory Donnell <donnell74@live.missouristate.edu>
2014-10-07 15:56:14 -0500

    Moved generate_starting_population to inside of run

--- run.py
+++ run.py
@@ -4,6 +4,4 @@ import globs
 
 if __name__ == "__main__":
     globs.init()
-    globs.mainScheduler.generate_starting_population()
-    print(globs.mainScheduler.weeks[0].print_concise())
     mainPage.main()
