b33e309025ee98a826f98f1ba22f9ecbcc0650c5
Hana Baldwin <hana9235@live.missouristate.edu>
2014-10-07 20:28:12 -0500

    update createConstraint function, fix course dropdown

--- gui/guiConstraints.py
+++ gui/guiConstraints.py
@@ -1,6 +1,11 @@
 from Tkinter import *
 from guiClasses import *
 from datetime import time
+import sys
+sys.path.append("../")
+import globs
+from genetic import constraint
+
 
 class Page(Frame):
     def __init__(self, root):
@@ -31,41 +36,47 @@ class CourseConstraint(Page):
         Frame.__init__(self, root)
 #         self.head_label = Label(self, text="Course Form")
 #         self.head_label.pack()
-#         
+#
+
+        globs.init()
+        
         message_course = Label(self, text="Course code:")
-        message_course.pack({"side": "top"})
+        message_course.pack(side = TOP)
 
         self.str_course_default = StringVar(self)
-        self.str_course_default.set("CSC450") 
-        self.option_course = OptionMenu(self, self.str_course_default, "CSC450","CSC333", "CSC232", "CSC325", "all")
-        self.option_course.pack({"side": "top"})
+        list_of_courses = globs.courses
+        list_of_courses.append("All")
+        self.str_course_default.set(list_of_courses[0]) 
+        self.option_course = OptionMenu(self, self.str_course_default, *list_of_courses)
+        self.option_course.pack(side = TOP)
         
         message_when = Label(self, text="When:")
-        message_when.pack({"side": "top"})
+        message_when.pack(side = TOP)
         
         self.str_when_default = StringVar(self)
         self.str_when_default.set("Before")
         self.option_when = OptionMenu(self, self.str_when_default, "Before", "After")
-        self.option_when.pack({"side": "top"})
+        self.option_when.pack(side = TOP)
         
         message_time = Label(self, text="Time:")
-        message_time.pack({"side": "top"})
+        message_time.pack(side = TOP)
         
         self.str_time_default = StringVar(self)
+        
         self.str_time_default.set("09:00")
         self.option_time = OptionMenu(self, self.str_time_default, "09:00", "10:00", "11:00", "12:00")
-        self.option_time.pack({"side": "top"})
+        self.option_time.pack(side = TOP)
         
         message_priority = Label(self, text="Priority:")
-        message_priority.pack({"side": "top"})
+        message_priority.pack(side = TOP)
         
         self.str_priority_default = StringVar(self)
         self.str_priority_default.set("Low") # initial value
         self.option_priority = OptionMenu(self, self.str_priority_default, "Low", "Medium", "High", "Mandatory")
-        self.option_priority.pack({"side": "top"})
+        self.option_priority.pack(side = TOP)
         
         self.button_go = Button(self, text="Add Constraint", command=self.go)
-        self.button_go.pack({"side": "right"})
+        self.button_go.pack(side = RIGHT)
         
     def go(self):
         course = self.str_course_default.get()
@@ -82,6 +93,7 @@ class ConstraintPage(Page):
         self.head_label.pack()
         self.create_widgets()
         self.pack(side = TOP, fill = "both")
+
         
         # CONTENT
         self.content_container = Frame(self, width="400", height="300")
@@ -128,21 +140,31 @@ def createConstraint(course, start_time, when, priority):
     hour, minute = start_time.split(":")
     time_obj = time( int(hour), int(minute) )
 
-    # MOSTLY PSEUDOCODE, TO BE IMPLEMENTED WHEN GUI AND SCHEDULER ARE LINKED
-    # if course != "all":  
+    if course != "All" or "all":
         #handle finding the course object in s.courses, assign it to course
-        # for c in s.courses:
-        #    if c.code == course:
-        #        course = c
-        # if when == "Before"   
-            # s.add_constraint(Constraint(constraint_name, priority, course_before_constraint, this_week, course, time ) )
-        # else:  # one course AFTER a time
-            # add course_after_constraint(this_week, course, time)
-    # else: # applies to all courses
-        # if when == "before"
-            # add all_before_constraint(this_week, time)
-        # else: # all courses AFTER
-            # add all_after_constraint(this_week, time)
+        for c in globs.mainScheduler.courses:
+            if c.code == course:
+                course = c
+                print(type(course))
+                break        
+        if when == "Before":
+             globs.mainScheduler.add_constraint(constraint_name, priority,
+                                                constraint.course_before_time,
+                                                 [course, time]) 
+        else:  # one course AFTER a time
+             globs.mainScheduler.add_constraint(constraint_name, priority,
+                                        constraint.course_after_time,
+                                         [course, time]) 
+    else: # applies to all courses
+        course = globs.mainScheduler.courses
+        if when == "Before":
+             globs.mainScheduler.add_constraint(constraint_name, priority,
+                                                constraint.all_before_time,
+                                                 [course, time]) 
+        else: # all courses AFTER
+             globs.mainScheduler.add_constraint(constraint_name, priority,
+                                                constraint.all_before_time,
+                                                 [course, time]) 
     print constraint_name, "priority/weight = " + str(priority)
     return 
     
