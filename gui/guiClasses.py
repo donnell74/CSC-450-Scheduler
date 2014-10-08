b44bd39556dcc97725bff6b98179cd94f55a666f
Cameron King <king852@live.missouristate.edu>
2014-10-07 22:35:40 -0500

    no longer test morning_classes

--- gui/guiClasses.py
+++ gui/guiClasses.py
@@ -145,9 +145,9 @@ class MainWindow(Frame):
 
     def run_scheduler(self):
         # RUN SCHEDULER METHOD
-        globs.mainScheduler.add_constraint("morning_classes", 30, constraint.morning_class, [globs.mainScheduler.courses[0]]) 
-        globs.mainScheduler.add_constraint("morning_classes", 30, constraint.morning_class, [globs.mainScheduler.courses[1]]) 
-        globs.mainScheduler.add_constraint("morning_classes", 30, constraint.morning_class, [globs.mainScheduler.courses[2]])
+        #globs.mainScheduler.add_constraint("morning_classes", 30, constraint.morning_class, [globs.mainScheduler.courses[0]]) 
+        #globs.mainScheduler.add_constraint("morning_classes", 30, constraint.morning_class, [globs.mainScheduler.courses[1]]) 
+        #globs.mainScheduler.add_constraint("morning_classes", 30, constraint.morning_class, [globs.mainScheduler.courses[2]])
         globs.mainScheduler.add_constraint("instructor conflict", 0, constraint.instructor_conflict, globs.instructors)
         globs.mainScheduler.evolution_loop()
         interface.export_schedules(globs.mainScheduler.weeks)
