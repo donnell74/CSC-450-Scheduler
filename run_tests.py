65831ddadffe9009dde8e2438540275a8240e7f4
Gregory Donnell <donnell74@live.missouristate.edu>
2014-10-22 17:50:52 -0500

    Fixed run tests to return exit code

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,12 @@
 import unittest
+import sys
 
-test_loader = unittest.TestLoader()
-tests = test_loader.discover('tests')
-test_runner = unittest.runner.TextTestRunner()
-test_runner.run(tests)
+
+if __name__ == "__main__":
+    test_loader = unittest.TestLoader()
+    tests = test_loader.discover('tests')
+    test_runner = unittest.runner.TextTestRunner()
+    result = test_runner.run(tests)
+    status = len(result.errors) + len(result.failures)
+
+    sys.exit(status)
