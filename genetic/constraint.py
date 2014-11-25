0d0ff1a90fd61237688f8c480f566259d8cff2e4
Cameron King <king852@live.missouristate.edu>
2014-11-25 12:08:09 -0600

    fixes wrong returned values on 3 constraints when mandatory; consolidates lab constraints into 1 constraint

--- genetic/constraint.py
+++ genetic/constraint.py
@@ -86,7 +86,12 @@ def course_after_time(this_week, args):
 
 
 def lab_on_tr(this_week, args):
-    return int(this_week.find_course(args[0])[0].isTR == True)
+    lab_courses = args[0]
+    for each_lab in lab_courses:
+        if not this_week.find_course(each_lab)[0].isTR:
+            this_week.valid = False
+            return 0
+    return 1
 
 
 def morning_class(this_week, args):
@@ -256,7 +261,7 @@ def instructor_preference_day(this_week, args):
                     
     # if mandatory and it's here, it passed
     if is_mandatory:
-        return 0
+        return 1
     else: # not mandatory, partial credit
         partial_credit = get_partial_credit(holds)
         return partial_credit
@@ -296,7 +301,7 @@ def instructor_preference_computer(this_week, args):
 
     # if mandatory and it's here, it passed
     if is_mandatory:
-        return 0
+        return 1
     else:
         partial_credit = get_partial_credit(holds)
         return partial_credit
@@ -333,7 +338,7 @@ def instructor_break_constraint(this_week, args):
             holds.append(1)
 
     if is_mandatory: # if no fails by here, it passed
-        return 0
+        return 1
     else:
         partial_weight = get_partial_credit(holds)
         return partial_weight
