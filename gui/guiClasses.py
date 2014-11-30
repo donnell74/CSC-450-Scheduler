e9fc8d881904dc8494d955102b6fd89c634f6e75
Renato Rangel <renatorangelbomfim@gmail.com>
2014-11-30 17:03:42 -0600

    Removed splash button and disabled buttons when the scheduling is running

--- gui/guiClasses.py
+++ gui/guiClasses.py
@@ -955,10 +955,10 @@ class MainWindow(Frame):
                                width="10", height="3", font=(font_style, size_h2), cursor = 'hand2')
         self.view_btn.pack(fill=X, side="top", pady=2)
 
-        self.misc_btn = Button(self.menu, text='Splash', command=self.show_misc, \
-                               width="10", height="3", font=(font_style, size_h2), cursor = 'hand2')
-        self.misc_btn.pack(fill=X, side="top", pady=2)
-
+#         self.misc_btn = Button(self.menu, text='Splash', command=self.show_misc, \
+#                                width="10", height="3", font=(font_style, size_h2), cursor = 'hand2')
+#         self.misc_btn.pack(fill=X, side="top", pady=2)
+#editing renato
         self.run_btn = Button(self.menu, text='RUN', bg='green', command=self.run_scheduler, \
                                width="10", height="3", font=(font_style, size_h2), cursor = 'hand2')
         self.run_btn.pack(fill = X, side = "top", pady=2)
@@ -991,6 +991,18 @@ class MainWindow(Frame):
     def show_misc(self):
         self.misc_page.lift()
 
+    def disable_buttons(self):
+        self.home_btn.config(state=DISABLED)
+        self.constraint_btn.config(state=DISABLED)
+        self.view_btn.config(state=DISABLED)
+        self.run_btn.config(state=DISABLED)
+
+    def enable_buttons(self):
+        self.home_btn.config(state=NORMAL)
+        self.constraint_btn.config(state=NORMAL)
+        self.view_btn.config(state=NORMAL)
+        self.run_btn.config(state=NORMAL)
+
     def thread_run_scheduler(self):
         instructors = globs.instructors
         self.misc_page.load_bar['width'] = 0 # make sure loading bar starts at 0 each run
@@ -1003,7 +1015,11 @@ class MainWindow(Frame):
                 runtime_var = 1
             print(runtime_var)
 
+        self.disable_buttons()
+
         globs.mainScheduler.evolution_loop(self, runtime_var)
+
+        self.enable_buttons()
 
         self.run_finished = True
         return
